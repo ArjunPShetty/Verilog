91ac694889c7a9b62ca071946b99ad0c7f74a45f
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2025-11-13 07:12:11 +0530

    vio folders

--- 4-Bit Full Adder/4-Bit Full Adder.ip_user_files/sim_scripts/questa/elaborate.do
+++
@@ -1,1 +0,0 @@
-vopt -l elaborate.log +acc=npr -suppress 10016  -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip -work xil_defaultlib xil_defaultlib.four_bit xil_defaultlib.glbl -o four_bit_opt
---
+++ VIO for Verification/VIO verification.srcs/sources_1/new/gates.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.11.2025 06:54:59
+// Design Name: 
+// Module Name: gates
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gates(
+    input a,b,
+    output y
+    );
+    assign y = a&b;
+endmodule
