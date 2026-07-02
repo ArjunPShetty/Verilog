11d35dbb8fce9a48d265e93de5b255bc5c88a347
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-07-03 05:04:44 +0530

    modified the case code

---
+++ case/testbench.v
@@ -0,0 +1,28 @@
+module tb;
+reg [1:0]a, b, c, d;
+reg [1:0]s;
+wire [1:0]y;
+
+cases uut(
+    .a(a),
+    .b(b),
+    .c(c),
+    .d(d),
+    .s(s),
+    .y(y)
+);
+
+initial begin
+    $dumpfile("dump.vvp");
+    $dumpvars(0,tb);
+end
+
+initial begin
+    a <= 2'b00; b <=2'b01; c <=2'b10; d <=2'b11; #100;
+    s <= 2'b00; #100;
+    s <= 2'b01; #100;
+    s <= 2'b10; #100;
+    s <= 2'b11; #100;
+    $finish;
+end
+endmodule
